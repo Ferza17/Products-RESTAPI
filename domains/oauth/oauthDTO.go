54c6f830432ef9a4b3bed794c13a12fe158de4e3
ferza17 <feryreza85@gmail.com>
2021-01-28 14:41:11 +0700

    add info token

--- domains/oauth/oauthDTO.go
+++ domains/oauth/oauthDTO.go
@@ -4,5 +4,6 @@ type Oauth struct {
 	IdToken    string `json:"id_token"`
 	CustomerId string `json:"customer_id"`
 	OrderId    string `json:"order_id"`
+	// varchar length(250)
 	Token      string `json:"token"`
 }
